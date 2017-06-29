f7f57e140ce6873dd5418c37b5daa7d63bdfca31
Lukas Hechenberger <admin@ls-age.com>
2017-06-29 09:36:55 +0200

    chore(release): Remove generated files [ci skip]

--- out/rules/strict.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var rules = exports.rules = {
-  // babel inserts `'use strict';` for us
-  strict: ['error', 'never']
-};
