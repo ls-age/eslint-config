8ef41afc407315a03d152238b6b2a97d3cbe8a01
CircleCI <circleci@circleci>
2017-11-22 12:33:39 +0000

    chore(release): Add v0.5.0-beta.0 release files [ci skip]

---
+++ out/rules/strict.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var rules = exports.rules = {
+  // babel inserts `'use strict';` for us
+  strict: ['error', 'never']
+};
