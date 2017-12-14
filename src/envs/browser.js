43be226292f84218c60fc9879ecc4eabd0272013
Lukas Hechenberger <admin@ls-age.com>
2017-12-14 09:08:35 +0100

    feat: Test browser compatibility using the `compat` plugin

---
+++ src/envs/browser.js
@@ -0,0 +1,19 @@
+export const env = {
+  browser: true,
+};
+
+export const plugins = [
+  'compat',
+];
+
+export const globals = {
+  asdf: false,
+};
+
+export const settings = {
+  browsers: ['> 1%'],
+};
+
+export const rules = {
+  'compat/compat': 'error',
+};
